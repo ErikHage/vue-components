001c3d7e87b47412a3ee970e9c051de5d9aa1941
Erik Hage <ehage4@gmail.com>
2024-11-14 19:20:05 -0500

    start building some rich dialog components

---
+++ src/dialog/ActivatorDialog.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-dialog v-model="dialog" max-width="500px">
+
+    <template v-slot:activator="{ props }">
+      <v-btn v-bind="props" color="primary">
+        {{ buttonText }}
+      </v-btn>
+    </template>
+
+    <slot></slot>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "ActivatorDialog",
+
+  props: {
+    buttonText: String,
+    showDialog: Boolean,
+  },
+
+  computed: {
+    dialog: this.showDialog,
+  },
+
+  methods: {
+    handleAction() {
+      console.log('Action confirmed!');
+      this.dialog = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
