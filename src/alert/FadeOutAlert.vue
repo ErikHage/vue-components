93341e0e998f2fe109ebb6242c12e7d05fca41dd
Erik Hage <ehage4@gmail.com>
2024-11-24 22:09:23 -0500

    Add FadeOutAlert component

---
+++ src/alert/FadeOutAlert.vue
@@ -0,0 +1,13 @@
+<template>
+  <transition name="fade">
+    <v-alert v-if="isVisible" :type="alertType" class="mt-3">{{ message }}</v-alert>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "FadeOutAlert",
+
+  props: ['isVisible', 'alertType', 'message'],
+}
+</script>
