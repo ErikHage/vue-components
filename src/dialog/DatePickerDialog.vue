6561b08899d7087e0598d2ed47a57af565f5d00f
Erik Hage <ehage4@gmail.com>
2024-11-25 22:17:53 -0500

    Add DatePickerDialog component

---
+++ src/dialog/DatePickerDialog.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-dialog v-model="showDialog" max-width="500px" persistent>
+    <v-card>
+      <v-card-title>
+        <span class="headline">{{ title }}</span>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-date-picker
+              v-model="selectedDate"
+              label="Select Date"
+              required
+              locale="en"
+              full-width
+          ></v-date-picker>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="red darken-1" text @click="handleOnCancel">Cancel</v-btn>
+        <v-btn color="blue darken-1" text @click="handleOnSubmit">Ok</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'DatePickerDialog',
+
+  props: {
+    show: Boolean,
+    title: String,
+    date: Date,
+    onSubmit: Function,
+    onCancel: Function,
+  },
+
+  data() {
+    return {
+      selectedDate: this.date || null,
+    }
+  },
+
+  computed: {
+    showDialog() {
+      return this.show;
+    },
+  },
+
+  methods: {
+    handleOnCancel() {
+      this.onCancel();
+    },
+
+    handleOnSubmit() {
+      this.onSubmit(this.selectedDate);
+    },
+  },
+}
+</script>
